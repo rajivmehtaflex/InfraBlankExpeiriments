e5bb40e4cda83f77d953ccba772d41fa87adab90
Rajiv Mehta <rajiv.mehta@live.in>
2023-01-24 09:33:43 +0000

    Add some updated version

--- aws-python-project/handler.py
+++ aws-python-project/handler.py
@@ -2,9 +2,12 @@ import json
 
 
 def hello(event, context):
+    
+    body=json.loads(event['Records'][0]['body'])
+    message=json.loads(body['Message'])    
+    print(f'Data -->{message}')
     body = {
-        "message": "Go Serverless v3.0! Your function executed successfully!",
-        "input": event,
+        "message": "Go Serverless v3.0! Your function executed successfully!"
     }
 
     return {"statusCode": 200, "body": json.dumps(body)}
